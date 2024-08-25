138bbe4c7144e1b5b069da081b77ee0d2d308da8
krajat03 <rajatunsar@gmail.com>
2024-08-25 14:58:52 +0530

    data structure

---
+++ linear_search.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int key;
+    cout << "Key: ";
+    cin >> key;
+    cout << endl;
+
+    int index = -1;
+    int array[5] = {1, 2, 8, 10, 16};
+    int length = sizeof(array) / sizeof(int);
+
+    for (int i = 0; i < length; i++)
+    {
+        if (array[i] == key)
+        {
+            index = i;
+            break;
+        }
+    }
+
+    if (index != -1)
+    {
+        cout << "The index is: " << index;
+    }
+    else
+    {
+        cout << "Invalid Key";
+    }
+
+    return 0;
+}
