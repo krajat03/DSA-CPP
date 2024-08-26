51b13e86b385b6d6b5c480a6d10ad40256f68c46
krajat03 <rajatunsar@gmail.com>
2024-08-26 10:50:13 +0530

    binary question 1

--- first_and_last_occurance.cpp
+++ first_and_last_occurance.cpp
@@ -1,18 +1,59 @@
 #include <iostream>
 using namespace std;
+int firstOcc(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+    int mid = start + ((end - start) / 2);
+    int ans = -1;
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            end = mid - 1;
+        }
+        else if (key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else if (key < arr[mid])
+        {
+            end = mid - 1;
+        }
+        mid = start + ((end - start) / 2);
+    }
+    return ans;
+}
+int lastOcc(int arr[], int size, int key)
+{
+    int start = 0;
+    int end = size - 1;
+    int mid = start + ((end - start) / 2);
+    int ans = -1;
+    while (start <= end)
+    {
+        if (arr[mid] == key)
+        {
+            ans = mid;
+            start = mid + 1;
+        }
+        else if (key > arr[mid])
+        {
+            start = mid + 1;
+        }
+        else if (key < arr[mid])
+        {
+            end = mid - 1;
+        }
+        mid = start + ((end - start) / 2);
+    }
+    return ans;
+}
 int main()
 {
-    int arr[8] = {0, 0, 1, 1, 2, 2, 2, 2};
-    int length = sizeof(arr) / sizeof(int);
-    for (int i = 0; i < length; i++)
-    {
-        if (arr[i] == 2)
-        {
-            cout << i << " ";
-            break;
-        }
-        
-    }
-    
+    int even[7] = {0, 1, 1, 2, 2, 2, 4};
+    cout << "First ocuurance of 2 at index: "<< firstOcc(even, 7, 2) << endl;
+    cout << "Last ocuurance of 2 at index: "<< lastOcc(even, 7, 2);
     return 0;
 }
